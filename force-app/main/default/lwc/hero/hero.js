9dc3c80eec37d2282e9d857af94490e27cb88ae4
Clinton <clintondsouza80@gmail.com>
2023-07-24 22:00:15 +0530

    Addes hero section LWC to our Portfolio site

---
+++ force-app/main/default/lwc/hero/hero.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+import IMAGES from '@salesforce/resourceUrl/Images'
+
+export default class Hero extends LightningElement {
+
+    imageURL = IMAGES + '/pexels-andrea-piacquadio-774909.jpg'
+}
