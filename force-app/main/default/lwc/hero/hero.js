17871a73ec1a9c84e74cac5c40488f1354bdbb78
Clinton <clintondsouza80@gmail.com>
2023-07-25 01:02:57 +0530

    Completed till submitForm function

--- force-app/main/default/lwc/hero/hero.js
+++ force-app/main/default/lwc/hero/hero.js
@@ -4,4 +4,24 @@ import IMAGES from '@salesforce/resourceUrl/Images'
 export default class Hero extends LightningElement {
 
     imageURL = IMAGES + '/pexels-andrea-piacquadio-774909.jpg'
+
+    dialog;
+
+    renderedCallback(){
+        this.dialog = this.template.querySelector('.leadForm');
+    }
+
+    showDialog(){
+        this.dialog.showModal();
+
+    }
+
+    closeDialog(){
+        this.dialog.close();
+
+    }
+
+    submitForm(){
+        //stopped!!!!!!!!!
+    }
 }
---
+++ force-app/main/default/lwc/leadDialog/leadDialog.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class LeadDialog extends LightningElement {}
