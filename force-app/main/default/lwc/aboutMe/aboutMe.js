21eced330c99eb69c45ea7621b29c7c4861b1850
Clinton <clintondsouza80@gmail.com>
2023-07-27 23:59:26 +0530

    Committing Before final touch but product works

---
+++ force-app/main/default/lwc/aboutMe/aboutMe.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+import IMAGES from '@salesforce/resourceUrl/Images';
+
+export default class AboutMe extends LightningElement {
+    aboutMeIMG = IMAGES + '/testimonial-sx-ema1-open.jpg';
+
+}
